09afa581ae334d1ac51adf160244897db89294f7
DESKTOP-35I757V\USER <ivanftp@gmail.com>
2022-04-30 14:52:37 +0800

    Add background process to pull crypto prices from binance

--- cefi.py
+++ cefi.py
@@ -2,6 +2,7 @@ import requests
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import asyncio
 
 app = FastAPI()
 
@@ -16,9 +17,30 @@ app.add_middleware(
 )
 
 
+class BackgroundProcess:
+    def __init__(self):
+        self.prices = {"BTCUSDT": None, "ETHUSDT": None, "LUNAUSDT": None, "BNBUSDT": None}
+
+    async def get_prices(self):
+        while True:
+            await asyncio.sleep(1)
+            for symbol in self.prices:
+                url = 'https://api.binance.com/api/v1/ticker/price?symbol={0}'.format(symbol)
+                response = requests.request("GET", url)
+                self.prices[symbol] = response.json()["price"]
+
+
+process = BackgroundProcess()
+
+
+@app.on_event('startup')
+async def app_startup():
+    asyncio.create_task(process.get_prices())
+
+
 @app.get("/")
-def read_root():
-    return {}
+def root():
+    return process.prices
 
 
 if __name__ == '__main__':
