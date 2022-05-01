62686db892b60bb3df3549b1ac4c310529507072
DESKTOP-35I757V\USER <ivanftp@gmail.com>
2022-05-01 23:55:10 +0800

    add unit tests

--- arbitrage/app.py
+++ arbitrage/app.py
@@ -17,31 +17,33 @@ def home():
     return render_template("home.html")
 
 
+def calculate_profit(buy_price, sell_price, buy_column_name, sell_column_name):
+    df = pd.concat(
+        [pd.DataFrame.from_dict(buy_price, orient='index', columns=[buy_column_name]).round(3),
+         pd.DataFrame.from_dict(sell_price, orient='index', columns=[sell_column_name]).round(3)], axis=1)
+    df['trade_size'] = (100000 / df[buy_column_name]).astype(int)
+    df['raw_profit'] = (
+            (df[sell_column_name] - df[buy_column_name]) *
+            df['trade_size']).round(2)
+    df['profit'] = df['raw_profit'].apply(
+        lambda x: format_currency(x, currency="USD", locale="en_US"))
+    df['profitable_trade'] = np.where(df['raw_profit'] > 0, True, False)
+    df.drop('raw_profit', 1, inplace=True)
+    df.index.name = 'crypto'
+    df = df.reset_index()
+    df[sell_column_name] = df[sell_column_name].apply(lambda x: '$' + str(x))
+    df[buy_column_name] = df[buy_column_name].apply(lambda x: '$' + str(x))
+    return df.T.to_json()
+
+
 @app.route("/buy_cex_sell_dex")
 def buy_cex_sell_dex():
     try:
         cex_buy_price = requests.get(cex_url + 'buy').json()
         dex_sell_price = requests.get(dex_url + 'sell').json()
-
-        df_buy_cex_sell_dex = pd.concat(
-            [pd.DataFrame.from_dict(cex_buy_price, orient='index', columns=['cex_buy_price']).round(3),
-             pd.DataFrame.from_dict(dex_sell_price, orient='index', columns=['dex_sell_price']).round(3)], axis=1)
-        df_buy_cex_sell_dex['trade_size'] = (100000 / df_buy_cex_sell_dex['cex_buy_price']).astype(int)
-        df_buy_cex_sell_dex['raw_profit'] = (
-                (df_buy_cex_sell_dex['dex_sell_price'] - df_buy_cex_sell_dex['cex_buy_price']) *
-                 df_buy_cex_sell_dex['trade_size']).round(2)
-        df_buy_cex_sell_dex['profit'] = df_buy_cex_sell_dex['raw_profit'].apply(
-            lambda x: format_currency(x, currency="USD", locale="en_US"))
-        df_buy_cex_sell_dex['profitable_trade'] = np.where(df_buy_cex_sell_dex['raw_profit'] > 0, True, False)
-        df_buy_cex_sell_dex.drop('raw_profit', 1, inplace=True)
-        df_buy_cex_sell_dex.index.name = 'crypto'
-        df_buy_cex_sell_dex = df_buy_cex_sell_dex.reset_index()
-        df_buy_cex_sell_dex['dex_sell_price'] = df_buy_cex_sell_dex['dex_sell_price'].apply(lambda x: '$' + str(x))
-        df_buy_cex_sell_dex['cex_buy_price'] = df_buy_cex_sell_dex['cex_buy_price'].apply(lambda x: '$' + str(x))
+        return calculate_profit(cex_buy_price, dex_sell_price, 'cex_buy_price', 'dex_sell_price')
     except Exception as e:
         print(e)
-
-    return df_buy_cex_sell_dex.T.to_json()
 
 
 @app.route("/buy_dex_sell_cex")
@@ -49,26 +51,10 @@ def buy_dex_sell_cex():
     try:
         cex_sell_price = requests.get(cex_url + 'sell').json()
         dex_buy_price = requests.get(dex_url + 'buy').json()
-
-        df_buy_dex_sell_cex = pd.concat(
-            [pd.DataFrame.from_dict(dex_buy_price, orient='index', columns=['dex_buy_price']).round(3),
-             pd.DataFrame.from_dict(cex_sell_price, orient='index', columns=['cex_sell_price']).round(3)], axis=1)
-        df_buy_dex_sell_cex['trade_size'] = (100000 / df_buy_dex_sell_cex['dex_buy_price']).astype(int)
-        df_buy_dex_sell_cex['raw_profit'] = (
-                (df_buy_dex_sell_cex['cex_sell_price'] - df_buy_dex_sell_cex['dex_buy_price']) *
-                df_buy_dex_sell_cex['trade_size']).round(2)
-        df_buy_dex_sell_cex['profit'] = df_buy_dex_sell_cex['raw_profit'].apply(
-            lambda x: format_currency(x, currency="USD", locale="en_US"))
-        df_buy_dex_sell_cex['profitable_trade'] = np.where(df_buy_dex_sell_cex['raw_profit'] > 0, True, False)
-        df_buy_dex_sell_cex.drop('raw_profit', 1, inplace=True)
-        df_buy_dex_sell_cex.index.name = 'crypto'
-        df_buy_dex_sell_cex = df_buy_dex_sell_cex.reset_index()
-        df_buy_dex_sell_cex['dex_buy_price'] = df_buy_dex_sell_cex['dex_buy_price'].apply(lambda x: '$'+str(x))
-        df_buy_dex_sell_cex['cex_sell_price'] = df_buy_dex_sell_cex['cex_sell_price'].apply(lambda x: '$' + str(x))
+        return calculate_profit(dex_buy_price, cex_sell_price, 'dex_buy_price', 'cex_sell_price')
     except Exception as e:
         print(e)
 
-    return df_buy_dex_sell_cex.T.to_json()
 
 
 if __name__ == '__main__':
---
+++ arbitrage/tests/test_calculate_profit.py
@@ -0,0 +1,15 @@
+from arbitrage.app import calculate_profit
+import json
+
+
+def test_calculate_profit():
+    cex_buy_price = {'ETHUSDT': 2789.26, '1INCHUSDT': 1.146, 'AAVEUSDT': 144.9, 'UNIUSDT': 7.06, 'LINKUSDT': 11.2}
+    dex_sell_price = {'AAVEUSDT': 144.50244696042617, '1INCHUSDT': 1.1421777116809815, 'LINKUSDT': 11.159650326998841,
+                      'ETHUSDT': 2790.980980194445, 'UNIUSDT': 7.038055503576642}
+    result = json.loads(calculate_profit(cex_buy_price, dex_sell_price, 'cex_buy_price', 'dex_sell_price'))
+    assert (type(result['0']['profitable_trade']) == bool) \
+           and (result['0']['crypto'] in cex_buy_price) \
+           and (type(result['0']['trade_size']) == int) \
+           and result['0']['cex_buy_price'].startswith('$') \
+           and result['0']['dex_sell_price'].startswith('$') \
+           and type(result['0']['profit']) == str
