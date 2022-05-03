9dce8abf6cdb1db265948a456a96bc4f8e5a30ce
DESKTOP-35I757V\USER <ivanftp@gmail.com>
2022-05-03 14:26:33 +0800

    add more unit tests

--- cefi/tests/test_fetch_cefi_prices.py
+++ cefi/tests/test_fetch_cefi_prices.py
@@ -15,7 +15,7 @@ def is_float(item):
     [requests.get("https://api.binance.com/api/v1/depth?symbol=ETHUSDT").json()],
     ids=["str_data"],
 )
-def test_bid_price(test_data):
+def test_eth_bid_price(test_data):
     # pull data from binance and test whether bid price and volume is a float
     bid = test_data['bids'][0]
     assert is_float(bid[0]) and is_float(bid[1])
@@ -26,7 +26,95 @@ def test_bid_price(test_data):
     [requests.get("https://api.binance.com/api/v1/depth?symbol=ETHUSDT").json()],
     ids=["str_data"],
 )
-def test_ask_price(test_data):
+def test_eth_ask_price(test_data):
+    # pull data from binance and test whether ask price and volume is a float
+    ask = test_data['asks'][0]
+    assert is_float(ask[0]) and is_float(ask[1])
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.binance.com/api/v1/depth?symbol=AAVEUSDT").json()],
+    ids=["str_data"],
+)
+def test_aave_bid_price(test_data):
+    # pull data from binance and test whether bid price and volume is a float
+    bid = test_data['bids'][0]
+    assert is_float(bid[0]) and is_float(bid[1])
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.binance.com/api/v1/depth?symbol=AAVEUSDT").json()],
+    ids=["str_data"],
+)
+def test_aave_ask_price(test_data):
+    # pull data from binance and test whether ask price and volume is a float
+    ask = test_data['asks'][0]
+    assert is_float(ask[0]) and is_float(ask[1])
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.binance.com/api/v1/depth?symbol=UNIUSDT").json()],
+    ids=["str_data"],
+)
+def test_uni_bid_price(test_data):
+    # pull data from binance and test whether bid price and volume is a float
+    bid = test_data['bids'][0]
+    assert is_float(bid[0]) and is_float(bid[1])
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.binance.com/api/v1/depth?symbol=UNIUSDT").json()],
+    ids=["str_data"],
+)
+def test_uni_ask_price(test_data):
+    # pull data from binance and test whether ask price and volume is a float
+    ask = test_data['asks'][0]
+    assert is_float(ask[0]) and is_float(ask[1])
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.binance.com/api/v1/depth?symbol=LINKUSDT").json()],
+    ids=["str_data"],
+)
+def test_link_bid_price(test_data):
+    # pull data from binance and test whether bid price and volume is a float
+    bid = test_data['bids'][0]
+    assert is_float(bid[0]) and is_float(bid[1])
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.binance.com/api/v1/depth?symbol=LINKUSDT").json()],
+    ids=["str_data"],
+)
+def test_link_ask_price(test_data):
+    # pull data from binance and test whether ask price and volume is a float
+    ask = test_data['asks'][0]
+    assert is_float(ask[0]) and is_float(ask[1])
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.binance.com/api/v1/depth?symbol=1INCHUSDT").json()],
+    ids=["str_data"],
+)
+def test_1inch_bid_price(test_data):
+    # pull data from binance and test whether bid price and volume is a float
+    bid = test_data['bids'][0]
+    assert is_float(bid[0]) and is_float(bid[1])
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.binance.com/api/v1/depth?symbol=1INCHUSDT").json()],
+    ids=["str_data"],
+)
+def test_1inch_ask_price(test_data):
     # pull data from binance and test whether ask price and volume is a float
     ask = test_data['asks'][0]
     assert is_float(ask[0]) and is_float(ask[1])
--- defi/tests/test_fetch_defi_prices.py
+++ defi/tests/test_fetch_defi_prices.py
@@ -7,7 +7,7 @@ import pytest
     [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&amount=100000000000").json()],
     ids=["str_data"],
 )
-def test_from_token_amount(test_data):
+def test_eth_from_token_amount(test_data):
     # pull data from 1inch and test whether fromTokenAmount is a positive integer
     fromTokenAmount = test_data['fromTokenAmount']
     assert (fromTokenAmount.startswith('-') == False) and fromTokenAmount[1:].isdigit()
@@ -18,7 +18,95 @@ def test_from_token_amount(test_data):
     [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&amount=100000000000").json()],
     ids=["str_data"],
 )
-def test_to_token_amount(test_data):
+def test_eth_to_token_amount(test_data):
+    # pull data from 1inch and test whether toTokenAmount is a positive integer
+    toTokenAmount = test_data['toTokenAmount']
+    assert (toTokenAmount.startswith('-') == False) and toTokenAmount[1:].isdigit()
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9&amount=100000000000").json()],
+    ids=["str_data"],
+)
+def test_aave_from_token_amount(test_data):
+    # pull data from 1inch and test whether fromTokenAmount is a positive integer
+    fromTokenAmount = test_data['fromTokenAmount']
+    assert (fromTokenAmount.startswith('-') == False) and fromTokenAmount[1:].isdigit()
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9&amount=100000000000").json()],
+    ids=["str_data"],
+)
+def test_aave_to_token_amount(test_data):
+    # pull data from 1inch and test whether toTokenAmount is a positive integer
+    toTokenAmount = test_data['toTokenAmount']
+    assert (toTokenAmount.startswith('-') == False) and toTokenAmount[1:].isdigit()
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0x1f9840a85d5af5bf1d1762f925bdaddc4201f984&amount=100000000000").json()],
+    ids=["str_data"],
+)
+def test_uni_from_token_amount(test_data):
+    # pull data from 1inch and test whether fromTokenAmount is a positive integer
+    fromTokenAmount = test_data['fromTokenAmount']
+    assert (fromTokenAmount.startswith('-') == False) and fromTokenAmount[1:].isdigit()
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0x1f9840a85d5af5bf1d1762f925bdaddc4201f984&amount=100000000000").json()],
+    ids=["str_data"],
+)
+def test_uni_to_token_amount(test_data):
+    # pull data from 1inch and test whether toTokenAmount is a positive integer
+    toTokenAmount = test_data['toTokenAmount']
+    assert (toTokenAmount.startswith('-') == False) and toTokenAmount[1:].isdigit()
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0x514910771af9ca656af840dff83e8264ecf986ca&amount=100000000000").json()],
+    ids=["str_data"],
+)
+def test_link_from_token_amount(test_data):
+    # pull data from 1inch and test whether fromTokenAmount is a positive integer
+    fromTokenAmount = test_data['fromTokenAmount']
+    assert (fromTokenAmount.startswith('-') == False) and fromTokenAmount[1:].isdigit()
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0x514910771af9ca656af840dff83e8264ecf986ca&amount=100000000000").json()],
+    ids=["str_data"],
+)
+def test_link_to_token_amount(test_data):
+    # pull data from 1inch and test whether toTokenAmount is a positive integer
+    toTokenAmount = test_data['toTokenAmount']
+    assert (toTokenAmount.startswith('-') == False) and toTokenAmount[1:].isdigit()
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0x111111111117dc0aa78b770fa6a738034120c302&amount=100000000000").json()],
+    ids=["str_data"],
+)
+def test_1inch_from_token_amount(test_data):
+    # pull data from 1inch and test whether fromTokenAmount is a positive integer
+    fromTokenAmount = test_data['fromTokenAmount']
+    assert (fromTokenAmount.startswith('-') == False) and fromTokenAmount[1:].isdigit()
+
+
+@pytest.mark.parametrize(
+    "test_data",
+    [requests.get("https://api.1inch.io/v4.0/1/quote?fromTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&toTokenAddress=0x111111111117dc0aa78b770fa6a738034120c302&amount=100000000000").json()],
+    ids=["str_data"],
+)
+def test_1inch_to_token_amount(test_data):
     # pull data from 1inch and test whether toTokenAmount is a positive integer
     toTokenAmount = test_data['toTokenAmount']
     assert (toTokenAmount.startswith('-') == False) and toTokenAmount[1:].isdigit()
